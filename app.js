271f01a8ac2bf9ba2978eee56e8f1424becdfd11
Willmar Tolentino <willmartolentino>
2013-04-16 15:34:10 +0800

    Modified app.js and index.html

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-alert("Cool Codes JS!");
+var info = {
+    firstName: "Jerome",
+    lastName: "Coloma"
+};
+
+document.getElementById('first-name').innerHTML = info.firstName;
+document.getElementById('last-name').innerHTML = info.lastName;
+
+// var firstNameEl = document.getElementById("first-name");
+
+// firstNameEl.addEventListener('click', function () { firstNameEl.innerHTML = "Modified Mikko"; });
+
+
+
+// // alert("Cool Codes JS!");
+
+// // document.getElementById("first-name").innerHTML = "Modified Mikko";
+// // document.getElementById("last-name").innerHTML = "Modified Tolentino";
