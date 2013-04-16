29c12e7c7207cfc67659ab8126b177a09b004888
Willmar Tolentino <willmartolentino>
2013-04-16 16:22:42 +0800

    Finished Vcard Project

--- vcard.js
+++ vcard.js
@@ -25,3 +25,20 @@ var vcard = {
         }
     ]
 };
+
+document.getElementById("first-name").innerHTML = vcard.name.firstName;
+document.getElementById("last-name").innerHTML = vcard.name.lastName;
+
+document.getElementById("email-1").innerHTML = vcard.emails[0];
+document.getElementById("email-2").innerHTML = vcard.emails[1];
+document.getElementById("email-3").innerHTML = vcard.emails[2];
+
+document.getElementById("street").innerHTML = vcard.address.street;
+document.getElementById("city").innerHTML = vcard.address.city;
+document.getElementById("country").innerHTML = vcard.address.country;
+document.getElementById("postal-code").innerHTML = vcard.address.postalCode;
+
+document.getElementById("phone-1-area-code").innerHTML = vcard.phones[0]["area-code"];
+document.getElementById("phone-1-line-number").innerHTML = vcard.phones[0]["line-number"];
+document.getElementById("phone-2-area-code").innerHTML = vcard.phones[1]["area-code"];
+document.getElementById("phone-2-line-number").innerHTML = vcard.phones[1]["line-number"];
