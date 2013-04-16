ad8ab045473afe43a3ca38a21547ad7b5c98be99
Willmar Tolentino <willmartolentino>
2013-04-16 15:33:07 +0800

    Add Vcard Template

---
+++ vcard.js
@@ -0,0 +1,27 @@
+var vcard = {
+    name: {
+        firstName: 'Asta',
+        lastName: 'Mañana'
+    },
+    emails: [
+        'asta@obob.com',
+        'asta.m@bleh.net',
+        'manana.asta@blokarak.tk'
+    ],
+    address: {
+        street: 'Gloria St.',
+        city: 'Makati City',
+        country: 'Philippines',
+        postalCode: '1210'
+    },
+    phones: [
+        {
+            'area-code': '02',
+            'line-number': '673-2012'
+        },
+        {
+            'area-code': '046',
+            'line-number': '752-4112'
+        }
+    ]
+};
